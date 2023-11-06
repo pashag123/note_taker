18e4b62849cd1b854b62e4887cfabe59f749f3ec
pashag123 <pashag123@gmail.com>
2023-11-06 11:32:07 -0800

    indexroutes

---
+++ routes/indexroutes.js
@@ -0,0 +1,13 @@
+const router = require('express').Router();
+
+const htmlRoutes = require('./htmlroutes');
+
+const apiRoutes = require('./apiroutes');
+
+
+router.use('/api', apiRoutes)
+
+router.use('/', htmlRoutes)
+
+module.exports = router
+
