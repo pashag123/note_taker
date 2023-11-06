10505724d76fc37908500415d6d342e58a82aac5
pashag123 <pashag123@gmail.com>
2023-11-06 12:07:38 -0800

    progress5

--- routes/apiroutes.js
+++ routes/apiroutes.js
@@ -1,3 +1,26 @@
 const router = require('express').Router();
 const fs = require('fs')
- 
+const { readAndRemove, readFromFile, readAndAppend } = require('../helpers/fsUtils');
+
+
+function randomId() {
+    return (Math.floor((1 + Math.random()) * 0x10000)
+    .toString(16)
+    .substring(1)
+    );
+}
+
+
+
+router.get('/notes', function (req, res) {
+    readFromFile('./db/db.json').then(data => res.json(JSON.parse(data)))
+})
+
+
+
+
+
+
+
+
+module.exports = router
--- routes/indexroutes.js
+++ routes/indexroutes.js
@@ -5,7 +5,7 @@ const htmlRoutes = require('./htmlroutes');
 const apiRoutes = require('./apiroutes');
 
 
-// router.use('/api', apiRoutes)
+router.use('/api', apiRoutes)
 
 router.use('/', htmlRoutes)
 
